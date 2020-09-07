41c9aef6f73605e61389e0878d6ddfa35f13baba
shrutarv <shrutarv.awasthi@gmail.com>
2020-09-07 16:44:04 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -22,9 +22,9 @@ from skpp import ProjectionPursuitRegressor
 from sklearn import datasets, ensemble
 from sklearn.metrics import mean_squared_error
 
-def bootstrap(data):
+def bootstrap(data, number):
     boot = []
-    for i in range(300):
+    for i in range(number):
         b = resample(data, replace=True, n_samples=int(0.1*len(data)))
         boot.append(b)
     return boot
@@ -89,9 +89,9 @@ def get_split(dataset):
     for index in range(len(dataset[0])-1):
         for row in dataset:
             groups = test_split(index, row[index], dataset)
-            gini = gini_index(groups, class_values)
-            if gini < b_score:
-                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+           # gini = gini_index(groups, class_values)
+            #if gini < b_score:
+               # b_index, b_value, b_score, b_groups = index, row[index], gini, groups
     return {'index':b_index, 'value':b_value, 'groups':b_groups}
 
 # Create a terminal node value
@@ -240,14 +240,44 @@ def NN_1(train_features,train_labels,test_features):
     return dist
     
 def get_features(train_features, train_labels, test_features, test_labels):
+    var = []
+    dist_edge = []
+    meta_features = np.empty(shape=(len(test_features),5))
     bf = base_features(train_features,train_labels)
     dist = NN_1(train_features,train_labels,test_features)
-    depth, num_example = CART(train_features,train_labels, test_features, test_labels)
-    dic = {"Depth":depth, "number of samples":num_example, "distance to nearest neighbour":dist}
-    dic.update(bf)
-    return dic
+    depth, num_example, sample = CART(train_features,train_labels, test_features)
+    for i in range(len(sample)):
+        var.append(np.var(sample[i]))
+    for j in range(len(test_features)):
+        dist_edge.append(MARS(train_features,train_labels, test_features[i]))
+    depth = np.asarray(depth, dtype='float64')
+    depth = depth.reshape(len(depth),1)
+    num_example = np.asarray(num_example, dtype='float64')
+    num_example = num_example.reshape(len(depth),1)
+    dist = np.asarray(dist, dtype='float64')
+    dist = dist.reshape(len(depth),1)
+    var = np.asarray(var, dtype='float64')
+    var = var.reshape(len(depth),1)
+    dist_edge = np.asarray(dist_edge, dtype='float64')
+    dist_edge = dist_edge.reshape(len(depth),1)
+    meta_features = np.concatenate((depth, num_example, dist, var, dist_edge), axis=1)
+    dist_edge = np.asarray(dist_edge)
+    #dic = {"Depth":depth, "number of samples":num_example, "variance":var, "dist to nearest edge":dist_edge, "distance to nearest neighbour":dist}
+    #dic.update(bf)
+    return meta_features
     
-def CART(X_train, y_train, X_test, y_test):
+def MARS(train_features,train_labels, test_features):
+     # MARS Learning algorthm as landmarker
+    model = Earth()
+    model.fit(train_features,train_labels)
+    #print(model.trace())
+    #print(model.summary())
+    prediction_mars = model.predict(test_features.reshape(1,-1))
+    distance_edge = np.min(np.abs(prediction_mars - train_labels))
+    model.score(train_features, train_labels)
+    return distance_edge
+    
+def CART(X_train, y_train, X_test):
     estimator = DecisionTreeClassifier(max_leaf_nodes=20, random_state=0)
     estimator.fit(X_train, y_train)
     
@@ -306,6 +336,7 @@ def CART(X_train, y_train, X_test, y_test):
     # a group of samples. First, let's make it for the sample.
     depth = []
     example_leaf = []
+    samples = []
     for sample_id in range(len(leave_id)):
         #sample_id = 2
         node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
@@ -329,9 +360,58 @@ def CART(X_train, y_train, X_test, y_test):
             else:
                 index = t[:,feature[node_id]]>=threshold[node_id]
             t = t[index[:],:]
+        samples.append(t)
         depth.append(d)   
         example_leaf.append(len(t))
-    return depth, example_leaf
+    return depth, example_leaf, samples
+
+def get_SVR(train_features, train_labels,test_features):
+    # SVR
+    svr_rbf = SVR(kernel='rbf', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
+    svr_rbf.fit(train_features,train_labels)
+    svr_pred = svr_rbf.predict(test_features)
+    
+    return svr_pred
+
+def PPR(train_features,train_labels,test_features):
+    #PPR
+    estimator = ProjectionPursuitRegressor()
+    estimator.fit(train_features,train_labels)
+    ppr_pred = estimator.predict(test_features)
+    return ppr_pred
+
+def RandomForest(train_features,train_labels,test_features):
+    # Random Forest
+    rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    rf.fit(train_features, train_labels)
+    rf_predict = rf.predict(test_features)
+    return rf_predict
+
+def GB(train_features, train_labels,test_features):
+    #GB
+    reg = ensemble.GradientBoostingRegressor()
+    reg.fit(train_features,train_labels)
+    gb_predict = reg.predict(test_features)
+    return gb_predict
+
+def Lasso():
+     # Lasso
+    parameters = {'alpha': np.concatenate((np.arange(0.1,2,0.1), np.arange(2, 5, 0.5), np.arange(5, 25, 1)))}
+    lasso = Lasso()
+    gridlasso = GridSearchCV(lasso, parameters, scoring ='r2')
+    gridlasso.fit(train_features,train_labels)
+    gridlasso.predict(test_features)
+    scaler = StandardScaler()
+    scaler.fit(train_features)
+    sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l2'))
+    sel_.fit(scaler.transform(train_features), train_labels)
+    sel_.get_support()
+    train_score=gridlasso.score(train_features,train_labels)
+    
+    test_score=gridlasso.score(test_features,test_labels)
+    print ("training score:", train_score) 
+    print ("test score: ", test_score)
+    print("lasso best parameters:", gridlasso.best_params_)
 
 '''
 dataset = [[2.771244718,1.784783929,0],
@@ -351,100 +431,107 @@ print_tree(tree)
 data = pd.read_csv("S:/Job/Time Series analysis/Task3/Concrete_Data_new.csv")
 # Implement boot strapping
 data = data.values
-feature = data[:,0:7]
-labels = data[:,8]
-train_features, test_features, train_labels, test_labels = train_test_split(feature, labels, test_size = 0.25, random_state = 42)
-n_folds = 5
-max_depth = 15
-min_size = 10
-dep = 0
-scores, dep = evaluate_algorithm(data, decision_tree, n_folds, dep, max_depth, min_size)
+data = data[:int(0.9*len(data)),:]
+bootstrapped_samples = bootstrap(data, number=100)
+print("number of bootstrapped samples",len(bootstrapped_samples))
 
-# Random Forest
-rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
-rf.fit(train_features, train_labels)
-rf_predict = rf.predict(test_features)
-mse_rf = mean_squared_error(test_labels, rf.predict(test_features))
+dt = []
+for index in range(len(bootstrapped_samples)):
+    data = bootstrapped_samples[index]
+    feature = data[:,0:7]
+    labels = data[:,8]
+    train_features, test_features, train_labels, test_labels = train_test_split(feature, labels, test_size = 0.25, random_state = 42)
+    n_folds = 5
+    max_depth = 15
+    min_size = 10
+    dep = 0
+    #scores, dep = evaluate_algorithm(data, decision_tree, n_folds, dep, max_depth, min_size)
+    label = []
+    # Random Forest
+    rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    rf.fit(train_features, train_labels)
+    # SVR
+    svr_rbf = SVR(kernel='rbf', C=100, gamma='auto', degree=3, epsilon=.1)
+    svr_rbf.fit(train_features,train_labels)
+    #PPR
+    estimator = ProjectionPursuitRegressor()
+    estimator.fit(train_features,train_labels)
+    #GB
+    reg = ensemble.GradientBoostingRegressor()
+    reg.fit(train_features,train_labels)
+    for i in range(len(test_features)):
+                
+        rf_predict = rf.predict(test_features[i,:].reshape(1,-1))
+        mse_rf = mean_squared_error(test_labels[i].reshape(1,-1), rf_predict)
+            
+        svr_pred = svr_rbf.predict(test_features[i,:].reshape(1,-1))
+        mse_svr = mean_squared_error(test_labels[i].reshape(1,-1), svr_pred)
+            
+        ppr_pred = estimator.predict(test_features[i,:].reshape(1,-1))
+        mse_ppr = mean_squared_error(test_labels[i].reshape(1,-1), ppr_pred)
+          
+        gb_pred = estimator.predict(test_features[i,:].reshape(1,-1))
+        mse_gb = mean_squared_error(test_labels[i].reshape(1,-1), gb_pred)
+        
+        dist_ppr = np.linalg.norm(ppr_pred-test_labels[i].reshape(1,-1))
+        dist_svr = np.linalg.norm(svr_pred-test_labels[i].reshape(1,-1))
+        dist_rf = np.linalg.norm(rf_predict-test_labels[i].reshape(1,-1))
+        dist_gb = np.linalg.norm(gb_pred-test_labels[i].reshape(1,-1))
+        label.append(np.argmin([dist_ppr,dist_svr,dist_rf,dist_gb]))
+     
+    label = np.asarray(label, dtype='float64')
+    label = label.reshape(len(label),1)
+    meta_features = get_features(train_features, train_labels, test_features, test_labels)  
+    clf = DecisionTreeClassifier()
+    dt.append(clf.fit(meta_features,label))
+    
+    #Predict the response for test dataset
+    y_pred = clf.predict(meta_features[0].reshape(1,-1))
+    
+    '''
+    # Random Noise
+    noise = np.random.normal(0,1,(1030,9))
+    data = data + noise
+     
+    # CART
+    clf = tree.DecisionTreeRegressor()
+    clf = clf.fit(train_features,train_labels)
+    pred = clf.predict(test_features)
+    pred,depth = decision_tree(train_features[1:100,:],test_features[1:10,:],15,10,0)
+    #depth = clf.get_depth()
+    clf.get_n_leaves()
+    dec_paths = clf.decision_path(test_features)
+    # Main
+    meta_features = get_features(train_features,train_labels, test_features, test_labels)
+    '''
+# Testing
+prediction=[]
+pred_per_dt = []
+error = []
+test_set = data[int(0.9*len(data)):,:]
+l = test_set[:,8]
+train_features, test_features, train_labels, test_labels = train_test_split(test_set, l, test_size = 0.25, random_state = 42)
+features = get_features(train_features, train_labels, test_features, test_labels) 
+print("number of decision trees", len(dt)) 
+for i in range(len(dt)):
+    pred = dt[i].predict(features)
+    pred = np.asarray(pred,dtype='int64')
+    count = np.bincount(pred)
+    predictor_label = np.argmax(count)
+    if (predictor_label==0):
+        prediction = PPR(train_features, train_labels,test_features)  
+    elif(predictor_label==1):
+        prediction = get_SVR(train_features, train_labels,test_features)
+    elif(predictor_label==2):
+         prediction = RandomForest(train_features, train_labels,test_features)
+    elif(predictor_label==3):
+         prediction = GB(train_features, train_labels,test_features)
+    error.append(mean_squared_error(test_labels, prediction))      
+            
+    pred_per_dt.append(sum(prediction)/len(prediction))
 
-# SVR
-svr_rbf = SVR(kernel='rbf', C=100, gamma='auto', degree=3, epsilon=.1,
-               coef0=1)
-svr_rbf.fit(train_features,train_labels)
-svr_pred = svr_rbf.predict(test_features)
-mse_svr = mean_squared_error(test_labels, svr_rbf.predict(test_features))
-
-#PPR
-estimator = ProjectionPursuitRegressor()
-estimator.fit(train_features,train_labels)
-ppr_pred = estimator.predict(test_features)
-mse_ppr = mean_squared_error(test_labels, estimator.predict(test_features))
-
-
-#GB
-reg = ensemble.GradientBoostingRegressor()
-reg.fit(train_features,train_labels)
-mse_gb = mean_squared_error(test_labels, reg.predict(test_features))
-
-dist_ppr = np.linalg.norm(ppr_pred-test_labels)
-dist_svr = np.linalg.norm(svr_pred-test_labels)
-dist_rf = np.linalg.norm(rf_predict-test_labels)
-# Random Noise
-noise = np.random.normal(0,1,(1030,9))
-data = data + noise
-
-
-# MARS Learning algorthm as landmarker
-model = Earth()
-model.fit(train_features,train_labels)
-print(model.trace())
-print(model.summary())
-prediction_mars = model.predict(test_features)
-model.score(train_features, train_labels)
-
-# Lasso
-parameters = {'alpha': np.concatenate((np.arange(0.1,2,0.1), np.arange(2, 5, 0.5), np.arange(5, 25, 1)))}
-lasso = Lasso()
-gridlasso = GridSearchCV(lasso, parameters, scoring ='r2')
-gridlasso.fit(train_features,train_labels)
-gridlasso.predict(test_features)
-scaler = StandardScaler()
-scaler.fit(train_features)
-sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l2'))
-sel_.fit(scaler.transform(train_features), train_labels)
-sel_.get_support()
-train_score=gridlasso.score(train_features,train_labels)
-
-test_score=gridlasso.score(test_features,test_labels)
-print ("training score:", train_score) 
-print ("test score: ", test_score)
-print("lasso best parameters:", gridlasso.best_params_)
-
-
-
-# CART
-clf = tree.DecisionTreeRegressor()
-clf = clf.fit(train_features,train_labels)
-pred = clf.predict(test_features)
-pred,depth = decision_tree(train_features[1:100,:],test_features[1:10,:],15,10,0)
-#depth = clf.get_depth()
-clf.get_n_leaves()
-dec_paths = clf.decision_path(test_features)
-# Main
-meta_features = get_features(train_features,train_labels, test_features, test_labels)
-
-
-
-
-n_nodes_ = [t.tree_.node_count for t in rf.estimators_]
-left      = rf.tree_.children_left
-max_depth = list()
-for tree in rf.estimators_:
-    max_depth.append(tree.tree_.max_depth)
-n_leaves = np.zeros(1000, dtype=int)
-for i in range(1000):
-    n_nodes = rf.estimators_[i].tree_.node_count
-    # use left or right children as you want 
-    children_left = rf.estimators_[i].tree_.children_left
-    for x in range(n_nodes):
-        if children_left[x] == -1:
-            n_leaves[i] += 1
+mean_error = sum(error)/len(error)
+output = sum(pred_per_dt)/len(pred_per_dt)    
+print("Predicted value", output, "mean square error",mean_error)   
+        
+        
