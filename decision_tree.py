95a42c2c5ca28ee2684a20c155d167954c82bc0d
shrutarv <shrutarv.awasthi@gmail.com>
2020-10-05 15:00:29 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,8 @@
-# Split a dataset based on an attribute and an attribute value
+"""
+@author: Shrutarv Awasthi
+shrutarv.awasthi@gmail.com
+"""
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
@@ -22,194 +26,6 @@ from skpp import ProjectionPursuitRegressor
 from sklearn import datasets, ensemble
 from sklearn.metrics import mean_squared_error
 
-'''    
-def test_split(index, value, dataset):
-    left, right = list(), list()
-    for row in dataset:
-        if row[index] < value:
-            left.append(row)
-        else:
-            right.append(row)
-    return left, right
-
-def index(groups, classes):
-    # count all samples at split point
-    n_instances = float(sum([len(group) for group in groups]))
-    ind = 0.0
-    for group in groups:
-        size = float(len(group))
-        if size == 0:
-            continue
-        prob = []
-        score = 0.0
-        for class_val in classes:
-            p = [row[-1] for row in group].count(class_val) / size
-            score = (score + p*p)
-        prob.append(score*(size / n_instances))
-      
-# base model can be SVR, PPR or GB
-def Impurity(base_model,node, groups):
-    # Calculate loss function
-    
-    return 1
-    
-
-
-"""
-# Calculate the Gini index for a split dataset
-def gini_index(groups, classes):
-    # count all samples at split point
-    n_instances = float(sum([len(group) for group in groups]))
-    # sum weighted Gini index for each group
-    gini = 0.0
-    for group in groups:
-        size = float(len(group))
-        # avoid divide by zero
-        if size == 0:
-            continue
-        score = 0.0
-        # score the group based on the score for each class
-        for class_val in classes:
-            p = [row[-1] for row in group].count(class_val) / size
-            score += p * p
-        # weight the group score by its relative size
-        gini += (1.0 - score) * (size / n_instances)
-    return gini
-"""
-# Select the best split point for a dataset
-def get_split(dataset):
-    class_values = list(set(row[-1] for row in dataset))
-    b_index, b_value, b_score, b_groups = 999, 999, 999, None
-    for index in range(len(dataset[0])-1):
-        for row in dataset:
-            groups = test_split(index, row[index], dataset)
-           # gini = gini_index(groups, class_values)
-            #if gini < b_score:
-               # b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-    return {'index':b_index, 'value':b_value, 'groups':b_groups}
-
-# Create a terminal node value
-def to_terminal(group):
-    outcomes = [row[-1] for row in group]
-    return max(set(outcomes), key=outcomes.count)
-
-# Create child splits for a node or make terminal
-def split(node, max_depth, min_size, depth):
-    left, right = node['groups']
-    del(node['groups'])
-    # check for a no split
-    if not left or not right:
-        node['left'] = node['right'] = to_terminal(left + right)
-        return
-    # check for max depth
-    if depth >= max_depth:
-        node['left'], node['right'] = to_terminal(left), to_terminal(right)
-        return
-    # process left child
-    if len(left) <= min_size:
-        node['left'] = to_terminal(left)
-    else:
-        node['left'] = get_split(left)
-        split(node['left'], max_depth, min_size, depth+1)
-    # process right child
-    if len(right) <= min_size:
-        node['right'] = to_terminal(right)
-    else:
-        node['right'] = get_split(right)
-        split(node['right'], max_depth, min_size, depth+1)
-
-# Build a decision tree
-def build_tree(train, max_depth, min_size):
-    root = get_split(train)
-    split(root, max_depth, min_size, 1)
-    return root
-
-# Print a decision tree
-def print_tree(node, depth=0):
-    if isinstance(node, dict):
-        print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1), node['value'])))
-        print_tree(node['left'], depth+1)
-        print_tree(node['right'], depth+1)
-    else:
-        print('%s[%s]' % ((depth*' ', node)))
-
-def accuracy_metric(actual, predicted):
-    correct = 0
-    for i in range(len(actual)):
-        if actual[i] == predicted[i]:
-            correct += 1
-    return correct / float(len(actual)) * 100.0
-
-def removearray(L,arr):
-    ind = 0
-    size = len(L)
-    while ind != size and not np.array_equal(L[ind],arr):
-        ind += 1
-    if ind != size:
-        L.pop(ind)
-    else:
-        raise ValueError('array not found in list.')
-    return L
-
-def cross_validation_split(dataset, n_folds):
-    dataset_split = list()
-    dataset_copy = list(dataset)
-    fold_size = int(len(dataset) / n_folds)
-    for i in range(n_folds):
-        fold = list()
-        while len(fold) < fold_size:
-            index = randrange(len(dataset_copy))
-            fold.append(dataset_copy.pop(index))
-        dataset_split.append(fold)
-    return dataset_split
-
-# Evaluate an algorithm using a cross validation split
-def evaluate_algorithm(dataset, algorithm, n_folds,dep, *args):
-    folds = cross_validation_split(dataset, n_folds)
-    scores = list()
-    for fold in folds:
-        train_set = list(folds)
-        train_set = removearray(train_set, fold)
-        train_set = sum(train_set, [])
-        test_set = list()
-        for row in fold:
-            row_copy = list(row)
-            test_set.append(row_copy)
-            row_copy[-1] = None
-        predicted, depth = algorithm(train_set, test_set, *args, dep)
-        actual = [row[-1] for row in fold]
-        accuracy = accuracy_metric(actual, predicted)
-        scores.append(accuracy)
-    return scores, depth
-
-def predict(node, row, dep):
-    if row[node['index']] < node['value']:
-        if isinstance(node['left'], dict):
-            dep += 1
-            return predict(node['left'], row, dep)
-        else:
-            dep += 1
-            return node['left'], dep
-    else:
-        if isinstance(node['right'], dict):
-            dep += 1
-            return predict(node['right'], row, dep)
-        else:
-            dep += 1
-            return node['right'], dep
-
-# Classification and Regression Tree Algorithm
-def decision_tree(train, test, max_depth, min_size, dep):
-    tree = build_tree(train, max_depth, min_size)
-    predictions = list()
-    depths = []
-    for row in test:
-        prediction, dep = predict(tree, row, dep)
-        predictions.append(prediction)
-        depths.append(dep)
-        dep = 0
-    return(predictions, depths)
-'''
 def bootstrap(data, number):
     boot = []
     for i in range(number):
@@ -227,28 +43,23 @@ def base_features(test_features):
     '''
     base = test_features
     return base
-
-# landmarker
-def NN_1(train_features,train_labels,test_features):
-        # 1 NN
-    knn = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
-    knn.fit(train_features,train_labels)
-    dist, ind = knn.kneighbors(test_features)
-    #zip_iterator = zip(ind.tolist(),dist.tolist())
-    #dictionary = dict(zip_iterator)
-    return dist
     
 def get_features(train_features, train_labels, test_features):
     var = []
     dist_edge = []
     meta_features = np.empty(shape=(len(test_features),5))
     bf = test_features
+    # Calling function NN_1 gives the absolute distance to nearest neighbour. 
     dist = NN_1(train_features,train_labels,test_features)
+    # Calling CART functions gives the depth of the leaf which makes the
+    # prediction, number of examples in that leaf and the value of these examples.
     depth, num_example, sample = CART(train_features,train_labels, test_features)
     for i in range(len(sample)):
         var.append(np.var(sample[i]))
+    # Calling MARS gives the distance to nearest edge
     for j in range(len(test_features)):
         dist_edge.append(MARS(train_features,train_labels, test_features[i]))
+    # All the features are reformatted and returned as a numpy array 
     depth = np.asarray(depth, dtype='float64')
     depth = depth.reshape(len(depth),1)
     num_example = np.asarray(num_example, dtype='float64')
@@ -260,10 +71,30 @@ def get_features(train_features, train_labels, test_features):
     dist_edge = np.asarray(dist_edge, dtype='float64')
     dist_edge = dist_edge.reshape(len(depth),1)
     meta_features = np.concatenate((depth, num_example, dist, var, dist_edge,bf), axis=1)
-    dist_edge = np.asarray(dist_edge)
     #dic = {"Depth":depth, "number of samples":num_example, "variance":var, "dist to nearest edge":dist_edge, "distance to nearest neighbour":dist}
     #dic.update(bf)
     return meta_features
+
+'''
+landmarker
+    1 nearest neighbour:
+    Inputs: train and test data with labels
+    Output : absolute distance to nearest neighbour
+'''
+def NN_1(train_features,train_labels,test_features):
+    knn = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
+    knn.fit(train_features,train_labels)
+    dist, ind = knn.kneighbors(test_features)
+    #zip_iterator = zip(ind.tolist(),dist.tolist())
+    #dictionary = dict(zip_iterator)
+    return dist
+
+'''
+landmarker
+    Multivariate adaptive regression splines (MARS):
+    Inputs: train and test data with labels
+    Output : distance to nearest edge
+'''
     
 def MARS(train_features,train_labels, test_features):
      # MARS Learning algorthm as landmarker
@@ -276,6 +107,15 @@ def MARS(train_features,train_labels, test_features):
     model.score(train_features, train_labels)
     return distance_edge
     
+'''
+landmarker
+    Classification And Regression Trees:
+    Inputs: X_train : train data
+            y_train : train labels
+            X_test : test data
+    Output : depth of the leaf which makes the prediction,
+            number of examples in that leaf, the value of these examples.
+'''
 def CART(X_train, y_train, X_test):
     #estimator = DecisionTreeClassifier(max_leaf_nodes=20, random_state=0)
     estimator = DecisionTreeRegressor(max_leaf_nodes=20, random_state=0)
@@ -366,15 +206,14 @@ def CART(X_train, y_train, X_test):
     return depth, example_leaf, samples
 
 def get_SVR(train_features, train_labels,test_features):
-    # SVR
+    # Support vector regression
     svr_rbf = SVR(kernel='rbf', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
     svr_rbf.fit(train_features,train_labels)
     svr_pred = svr_rbf.predict(test_features)
-    
     return svr_pred
 
 def PPR(train_features,train_labels,test_features):
-    #PPR
+    # Projection Pursuit regression
     estimator = ProjectionPursuitRegressor()
     estimator.fit(train_features,train_labels)
     ppr_pred = estimator.predict(test_features)
@@ -394,6 +233,10 @@ def GB(train_features, train_labels,test_features):
     gb_predict = reg.predict(test_features)
     return gb_predict
 
+'''
+Landmarker
+    This can also be used to extract meta features
+'''
 def Lasso():
      # Lasso
     parameters = {'alpha': np.concatenate((np.arange(0.1,2,0.1), np.arange(2, 5, 0.5), np.arange(5, 25, 1)))}
@@ -413,24 +256,30 @@ def Lasso():
     print ("test score: ", test_score)
     print("lasso best parameters:", gridlasso.best_params_)
 
-'''
-tree = build_tree(dataset, 1, 1)
-
-'''
 #################################################################################
 ######################################### MAIN ##################################
 #################################################################################
 if __name__ == '__main__':
         
     # Load the data. The below algorithm assumes that labels column should be the last one
-    data_orig = pd.read_csv("S:/Job/Time Series analysis/Task3/Concrete_Data_new.csv")
-    # Implement boot strapping
+    data_orig = pd.read_csv("S:/Job/Time Series analysis/Task3/Concrete_Data.csv")
+    # Implement boot strapping. The argument while calling bootstrap method decides the number
+    # of random bootstrap samples. It takes into account the location
     data_orig = data_orig.values
     data = data_orig[:int(0.9*len(data_orig)),:]
     bootstrapped_samples = bootstrap(data, number=100)
+    '''
+        # Random Noise
+        noise = np.random.normal(0,1,(1030,9))
+        data = data + noise
+    '''
+        
     print("number of bootstrapped samples",len(bootstrapped_samples))
     # Training
     dt = []         #stores the decision trees for each bootstrap
+    # We iterate over each bootstrap samples. For each sample we extract meta
+    # features, assign labels based on the best base learner. Then create a 
+    # decision tree for predicting the best base learner.
     for index in range(len(bootstrapped_samples)):
         print("bootstrap training sample"+ str(index))
         data = bootstrapped_samples[index]
@@ -442,7 +291,16 @@ if __name__ == '__main__':
         min_size = 10
         dep = 0
         #scores, dep = evaluate_algorithm(data, decision_tree, n_folds, dep, max_depth, min_size)
-        # Below code till line 471 is used to get the labels for each bootstrapped sample
+        '''
+        labels assigned as per
+        predictor                               index
+        ppr (projection pursuit regression):      0
+        svr (support vector regression):          1
+        rf (random forest):                       2
+        gb (gradient boosting):                   3
+        '''
+        # Below code till end of the inner for loop is used to get the 
+        # labels for each bootstrapped sample
         label = []
         # Random Forest
         rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
@@ -450,10 +308,10 @@ if __name__ == '__main__':
         # SVR
         svr_rbf = SVR(kernel='rbf', C=100, gamma='auto', degree=3, epsilon=.1)
         svr_rbf.fit(train_features,train_labels)
-        #PPR
+        # PPR
         estimator = ProjectionPursuitRegressor()
         estimator.fit(train_features,train_labels)
-        #GB
+        # GB
         reg = ensemble.GradientBoostingRegressor()
         reg.fit(train_features,train_labels)
         for i in range(len(test_features)):
@@ -469,7 +327,10 @@ if __name__ == '__main__':
               
             gb_pred = estimator.predict(test_features[i,:].reshape(1,-1))
             mse_gb = mean_squared_error(test_labels[i].reshape(1,-1), gb_pred)
-            
+            # We use the predictions from rf, gb, ppr and svr to calculate the 
+            # distance between the true values and prediction for each test sample.
+            # The label coorresponding to the predictor whose distance is least is 
+            # chosen as the label for that test sample.
             dist_ppr = np.linalg.norm(ppr_pred-test_labels[i].reshape(1,-1))
             dist_svr = np.linalg.norm(svr_pred-test_labels[i].reshape(1,-1))
             dist_rf = np.linalg.norm(rf_predict-test_labels[i].reshape(1,-1))
@@ -478,18 +339,14 @@ if __name__ == '__main__':
          
         label = np.asarray(label, dtype='float64')
         label = label.reshape(len(label),1)
+        # Extract the meta features 
         meta_features = get_features(train_features, train_labels, test_features)  
         # create a decision tree to select the best algorithm out of ppr, svr, gb and rf
         # use meta features and label to create decision tree
         clf = DecisionTreeClassifier()
         dt.append(clf.fit(meta_features,label))
      
-        '''
-        # Random Noise
-        noise = np.random.normal(0,1,(1030,9))
-        data = data + noise
-        '''
-        
+       
     # Testing
     prediction = 0.0
     predicted_values = []
@@ -508,13 +365,18 @@ if __name__ == '__main__':
         print(j)
         pred_pr_tree = []
         prediction = 0.0
-        # Inner loop predicts the output for each decision tree and then averages all the outputs
+        # Inner loop selects each bootstrapped decision tree and gives the selected 
+        # test sample from outer loop as the input. The predictions tell us which base learner is 
+        # the best predictor. 
+        # and then averages all the outputs
         for i in range(len(dt)):
             pred = dt[i].predict(features[j].reshape(1,-1))
             pred = np.asarray(pred,dtype='int64')
             count = np.bincount(pred)
-            # The best predictor is choosed. The predictions from them are summed and
-            # appended in prediction variable
+            # The best predictor is choosen and used to make predictions for the 
+            # selected test sample from outer loop. The predictions are summed, averaged
+            # and appended in 'prediction' variable. The prediction labels for every
+            # tree are stored in 'predictions' variable.
             predictor_label = np.argmax(count)
             if (predictor_label==0):
                 prediction += PPR(train_features, train_labels,test_features[j].reshape(1,-1))  
@@ -529,17 +391,18 @@ if __name__ == '__main__':
         predictions.append(pred_pr_tree)
         predicted_values.append(prediction/len(dt))           
        #pred_per_dt.append(sum(prediction)/len(prediction))
-    mse = mean_squared_error(test_labels, predicted_values)
+    mse = mean_squared_error(test_labels[:20], predicted_values[:20])
     std = np.asarray(predicted_values).std()
     # Base Learners
-    pred_ppr = PPR(train_features, train_labels,test_features)
-    mse_ppr = mean_squared_error(test_labels, pred_ppr)
+    # We compare the predictions of metabags we created with the below base learners.
+    pred_ppr = PPR(train_features, train_labels,test_features[:20,:])
+    mse_ppr = mean_squared_error(test_labels[:20], pred_ppr[:20])
     pred_svr = get_SVR(train_features, train_labels,test_features)
-    mse_svr = mean_squared_error(test_labels, pred_svr)
+    mse_svr = mean_squared_error(test_labels[:20], pred_svr[:20])
     pred_rf = RandomForest(train_features, train_labels,test_features)
-    mse_rf = mean_squared_error(test_labels, pred_rf)
+    mse_rf = mean_squared_error(test_labels[:20], pred_rf[:20])
     pred_gb = GB(train_features, train_labels,test_features)
-    mse_gb = mean_squared_error(test_labels, pred_gb)
+    mse_gb = mean_squared_error(test_labels[:20], pred_gb[:20])
     print("average error",mse) 
     # predictions variable contains the predicted labels for all the 100 bootstrapped
     # decision trees  for each test sample. mse_ variables are mean square errors.
